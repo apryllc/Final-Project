e28ab30e7782136e7fdf4c45aef708cc94f88eb3
Apryll <apryll.clark@gmail.com>
2018-07-19 18:25:48 -0400

    js updated with functioning jquery, includes place order message

--- js/app.js
+++ js/app.js
@@ -1,13 +1,18 @@
-var email = document.getElementById('email').value;
-document.getElementById('submitbtn').addEventListener('click', checkEmail);
-function checkEmail()
-{
-if (([/'@'/].test(email)) )
-    {
-        alert('Thanks for your order!')
+
+// jquery selects the submit button, then on that item listen for clicks and run the defined function //
+$("#submitbtn").on("click", function() {
+// variables declared (let is constant) //    
+    let email = $("#email");
+// below variable is a standard regular expression //    
+    let emailRegex = /^[@]+$/;
+// if-else loop //
+// test method checks the value of the email variable against the regular expression variable //    
+    if (emailRegex.test(email.val()) === true) {
+// if true //
+        alert('Thanks for your order!');
+    } 
+// if false //
+    else {
+      alert('Please enter a valid email address.');
     }
-    else
-    {
-        alert('Please enter a valid email address.')  
-        }
-}
+  });
