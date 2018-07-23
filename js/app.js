94951c2caf2e546e4a667e239ac6b9f5c5a66a8f
Apryll <apryll.clark@gmail.com>
2018-07-22 22:49:36 -0400

    updated input sizes in order form, updated comments in js

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
 
-// jquery selects the submit button, then on that item listen for clicks and run the defined function //
+// jquery selects the submit button, then on that item listen for clicks and runs the defined function //
 $("#submitbtn").on("click", function() {
 // variables declared (let is constant) //    
     let email = $("#email");
-// below variable is a standard regular expression //    
+// below variable is a standard regular expression //
+//escape char, ^ beginning of input, [] holds string, ^ negates everything in string, +@ in email, dollar size is closing char //
     let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 // if-else loop //
 // test method checks the value of the email variable against the regular expression variable //    
