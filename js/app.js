ddd78262fd51899ba3456ef4312c8dbd169b2685
Apryll <apryll.clark@gmail.com>
2018-07-24 00:17:33 -0400

    updated js to replace order form div with order success message if email passes

--- js/app.js
+++ js/app.js
@@ -10,8 +10,9 @@ $("#submitbtn").on("click", function(submit) {
 // test method checks the value of the email variable against the regular expression variable //    
     if (emailRegex.test(email.val()) === true) {
 // if true //
-        alert('Thanks for your order!');
-// prevents page from reloading //
+//selects order form div, replaces it with html message confirming order //
+        $(".orderform").html("<h3>Thanks for placing an order with Louisville's Best Brittle company!<br><br>Your order will be shipped in 2-3 business days to the address provided.<br><br>Happy snacking!</h3>");
+// prevents page from reloading
         submit.preventDefault();
     } 
 // if false //
